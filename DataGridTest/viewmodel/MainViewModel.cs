d2511df1426e5ec42ebd4a22d6d0a79c4ee00c6e
Hansi <hansisgithub@gmail.com>
2016-07-12 13:55:46 +0200

    start mit fertigem wpf aber ohne bindung

---
+++ DataGridTest/viewmodel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PropertyChanged;
+
+namespace DataGridTest.viewmodel
+{
+    
+    [ImplementPropertyChanged]
+    class MainViewModel
+    {
+        public ICommand ClickCommand { get; set; }
+    }
+}
